5ac2a3823c5f8abffe586f0901f641d97d664625
pettepiero <pette.piero@gmail.com>
2023-08-17 13:59:54 +0200

    Added matrix scaling bash file for OBLAS

---
+++ Assignment/exercise2/matr-scal-oblas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=EPYC
+#SBATCH --time=2:0:0
+#SBATCH -n 128
+#SBATCH -N1
+
+echo "Loading modules"
+module load mkl
+module load openBLAS/0.3.23-omp
+
+OPENBLASROOT=${OPENBLAS_ROOT}
+
+rm oblas-matr-output.txt
+rm counter.txt
+echo "Running executables..."
+output=$(srun -n1 --cpus-per-task=64 ./gemm_oblas.x)
+
+echo "$output" | grep "Currently" >> counter.txt
+echo "$output" | grep "GFLOPS" >> oblas-matr-output.txt
+
+echo "Finished."
