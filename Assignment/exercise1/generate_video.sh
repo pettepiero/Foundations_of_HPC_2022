e934b35c8611cfd106233655b23e94cb78d1a0a1
pettepiero <pette.piero@gmail.com>
2024-09-20 15:27:48 +0200

    Images are of bigger size now

--- Assignment/exercise1/generate_video.sh
+++ Assignment/exercise1/generate_video.sh
@@ -15,10 +15,14 @@ cd images/snapshots
 
 for i in $(ls $name*.pgm | sort -V); do
   echo "in loop, looking for $i"
-  output_file="converted/${name}_${index}.jpeg"
-  convert "$i" -quality 80 "$output_file"
+  output_file="converted/${name}_${index}.png"
+  
+  # Convert to strictly black and white, then resize using nearest-neighbor interpolation
+  convert "$i" -threshold 50% -resize 1280x -filter point -quality 80 "$output_file"
+  
   ((index++))
 done
 
 echo "Finished loop"
-ffmpeg -r 15 -start_number 0 -i "converted/${name}_%d.jpeg" -vcodec mpeg4 "output.mp4" -y
+ffmpeg -r 15 -start_number 0 -i "converted/${name}_%d.png" -vcodec mpeg4 "output.mp4" -y
+
