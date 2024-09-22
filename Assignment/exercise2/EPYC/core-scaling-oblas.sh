17974cae68372fcdd82fe08fb9fd3a19f2763f17
Piero <pette.piero@gmail.com>
2024-09-22 16:14:33 +0200

    Commit before merging to main

--- Assignment/exercise2/EPYC/core-scaling-oblas.sh
+++ Assignment/exercise2/EPYC/core-scaling-oblas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A dssc
-#SBATCH --job-name=MKL-core-scaling
+#SBATCH --job-name=oblas-core-scaling
 #SBATCH --partition=THIN
 #SBATCH --time=02:00:0
 #SBATCH --cpus-per-task=24
@@ -10,44 +10,49 @@
 #SBATCH --exclusive
 #SBATCH --output=./outputs/output_files/slurm-%j.txt
 
-echo "MKL core scaling"
+echo "OpenBLAS core scaling"
+echo "Expecting already compiled executables for THIN architecture"
 echo "********************************"
-export LD_LIBRARY_PATH=/u/dssc/ppette00/intel/oneapi/mkl/2024.2/lib/intel64:$LD_LIBRARY_PATH
-export OMP_PLACES=threads
-output_file="./outputs/core_scaling/mkl-core-scaling-$SLURM_JOB_ID.csv"
-matrix_size=10000
+echo "Loading modules"
+module load openBLAS/0.3.26-omp
 
+OPENBLASROOT=${OPENBLAS_ROOT}
+export OMP_PLACES=threads
+
+output_file="./outputs/core_scaling/oblas-core-scaling-$SLURM_JOB_ID.csv"
+matrix_size=10000
 # Add header
 echo "Measurement,Number of CPUs,Seconds,GFLOPS,Precision,Bind(threads)"> "$output_file"
+
 for binding in 'close' 'spread'
 do
 	export OMP_PROC_BIND=$binding
-	for ((j=1; j<=5;j+=1))
+	for ((j=1;j<=5;j+=1))
 	do
 		echo "Measurement $j"
 	
 		for ((i=1;i<=24;i+=1))
 		do
-		
+	
 			echo "Iteration $i"
-		
-			output=$(srun -n1 --cpus-per-task=$i ./gemm_mkl_single.x $matrix_size $matrix_size $matrix_size)
-		
+	
+			output=$(srun -n1 --cpus-per-task=$i ./gemm_oblas_single.x $matrix_size $matrix_size $matrix_size)
+	
 			# Extract relevant information
 			seconds=$(echo "$output" | tail -n 1 | awk '{print $2}')
 			gflops=$(echo "$output" | tail -n 1 | awk '{print $4}')
-		
+	
 			echo "$j,$i,$seconds,$gflops,Single,$binding" >> "$output_file"
-		
-			output=$(srun -n1 --cpus-per-task=$i ./gemm_mkl_double.x $matrix_size $matrix_size $matrix_size)
-		
+	
+			output=$(srun -n1 --cpus-per-task=$i ./gemm_oblas_double.x $matrix_size $matrix_size $matrix_size)
+	
 			# Extract relevant information
 			seconds=$(echo "$output" | tail -n 1 | awk '{print $2}')
 			gflops=$(echo "$output" | tail -n 1 | awk '{print $4}')
-		
+	
 			echo "$j,$i,$seconds,$gflops,Double,$binding" >> "$output_file"
-		
 		done
 	done
 done
 echo "Finished"
+
---
+++ Assignment/exercise2/EPYC/matrix-scaling-blis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH -A dssc
+#SBATCH --job-name=BLIS-matrix-scaling
+#SBATCH --partition=THIN
+#SBATCH --time=02:00:0
+#SBATCH --cpus-per-task=12
+#SBATCH --ntasks-per-node=1 
+#SBATCH --mem=200gb
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --output=./outputs/output_files/slurm-%j.txt
+
+echo "BLIS matrix scaling"
+echo "********************************"
+
+export LD_LIBRARY_PATH=/u/dssc/ppette00/myblis/lib:$LD_LIBRARY_PATH
+export BLIS_NUM_THREADS=12
+export OMP_NUM_THREADS=12
+export OMP_PLACES=threads
+output_file="./outputs/matrix_scaling/blis-matrx-scaling-$SLURM_JOB_ID.csv"
+
+# Add header
+echo "Measurement,Matrix Size (n),Seconds,GFLOPS,Precision,Bind"> "$output_file"
+
+for binding in 'close' 'spread'
+do
+	echo "Binding $binding"
+	export OMP_PROC_BIND=$binding
+	# Single precision scaling
+	for ((j=1; j<=5;j+=1))
+	do
+		echo "Measurement $j"
+	
+		for((i=2000; i <= 20000; i+=500))
+		do
+			echo "Iteration $i"
+		
+			out=$(srun -n1 --cpus-per-task=12 ./gemm_blis_single.x $i $i $i)
+			
+			# Extract the relevant information (seconds and GFLOPS)
+		        seconds=$(echo "$out" | tail -n 1 | awk '{print $2}')
+		        gflops=$(echo "$out" | tail -n 1 | awk '{print $4}')
+		
+			echo "$j,$i,$seconds,$gflops,Single,$binding" >> "$output_file"
+		
+			out=$(srun -n1 --cpus-per-task=12 ./gemm_blis_double.x $i $i $i)
+			
+			# Extract the relevant information (seconds and GFLOPS)
+		        seconds=$(echo "$out" | tail -n 1 | awk '{print $2}')
+		        gflops=$(echo "$out" | tail -n 1 | awk '{print $4}')
+		
+			echo "$j,$i,$seconds,$gflops,Double,$binding" >> "$output_file"
+		
+		done
+	done
+done
+#
+echo "Finished"
