08599d3ba9b7e5ae15b13a228f70e5dcc2b231a8
Piero <pette.piero@gmail.com>
2024-09-22 16:09:23 +0200

    created mpi-scalability script and improved openmp-scalability

--- Assignment/exercise1/openmp-scalability.sh
+++ Assignment/exercise1/openmp-scalability.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A dssc
 #SBATCH --job-name=openmp-scal
-#SBATCH --partition=EPYC
+#SBATCH --partition=THIN
 #SBATCH --time=02:00:0
 #SBATCH --exclusive
 #SBATCH --ntasks=1
@@ -9,14 +9,14 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=./outputs/slurm-%j-openmp-scal.txt
 
-module load openMPI/4.1.5/gnu/12.2.1
+module load openMPI/4.1.6/gnu/14.2.1
 
 echo "OpenMP scalability"
 echo "Running game of life with different #threads"
 echo "to collect data for time/threads plots"
 
 output_file="./outputs/timings/openmp-scal-$SLURM_JOB_ID.csv"
-max_num_threads=64    # number of cores in each AMD EPYC 7H12 CPU
+max_num_threads=24 
 
 
 
@@ -27,7 +27,6 @@ if [ "$1" == "-s" ]; then
     output_file="./outputs/timings/openmp-scal-$SLURM_JOB_ID-serial.csv"
 fi
 echo "dim,nthreads,time" > "$output_file"
-#100 500 1000 1500 2000
 for dim in 1000 1500 2000
 do
     # Check if single thread flag is provided
@@ -36,7 +35,7 @@ do
         echo "Running with a single thread"
 
         export OMP_NUM_THREADS=1
-        output=$(srun -n1 ./build/gol.x -i -k $dim)
+        output=$(srun ---ntasks-per-socket=1 ./build/gol.x -i -k $dim)
         time=$(echo "$output" | tail -n 1)
         echo "$dim,1,$time" >> "$output_file"
         echo "$output" >> "./outputs/slurm-$SLURM_JOB_ID-serial.txt"
@@ -49,7 +48,7 @@ do
             echo "Running with $i threads"
             export OMP_NUM_THREADS=$i
             
-            output=$(srun -n1 ./build/gol.x -i -k $dim)
+            output=$(srun --ntasks-per-socket=1 ./build/gol.x -i -k $dim)
 
             time=$(echo "$output" | tail -n 1)
             echo "$dim,$i,$time" >> "$output_file"
