68f72cbe78dafca37f90cf8c5d6d9201478a0051
Piero <pette.piero@gmail.com>
2024-09-27 11:02:20 +0200

    Makefile and sh scripts bug fixes

--- Assignment/exercise1/mpi-scalability.sh
+++ Assignment/exercise1/mpi-scalability.sh
@@ -2,40 +2,42 @@
 #SBATCH -A dssc
 #SBATCH --job-name=mpi-scal
 #SBATCH --partition=THIN
-#SBATCH --time=01:00:0
+#SBATCH --nodelist=thin[005-008]
+#SBATCH --time=00:05:0
 #SBATCH --ntasks-per-node=2 
 #SBATCH --cpus-per-task=12
 #SBATCH --nodes=4
 #SBATCH --exclusive
 #SBATCH --output=./outputs/slurm-%j-mpi-scal.txt
-
 module load openMPI/4.1.6/gnu/14.2.1
+# 0 -> ordered, 1 -> static
+evolution=1
+if [evolution==0]; then
+	echo "OpenMP scalability for ordered evolution"
+fi
+if [evolution==1]; then
+	echo "OpenMP scalability for static evolution"
+fi
 
-echo "MPI scalability"
+echo "MPI scalability for ordered evolution"
 echo "Running game of life with different #MPI tasks"
 echo "to collect data for time/tasks plots"
 
 output_file="./outputs/timings/mpi-scal-$SLURM_JOB_ID.csv"
-max_num_threads=12
-mapping="node"
-binding="socket"
-echo "mapping by $mapping, binding by $binding"
+mapping="socket"
+
+echo "mapping by $mapping"
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=12
 
-# Outer loop repeats the measurements for different matrix dimensions
-
-if [ "$1" == "-s" ]; then
-    output_file="./outputs/timings/openmp-scal-$SLURM_JOB_ID-serial.csv"
-fi
 echo "dim,mpi-tasks,time" > "$output_file"
-for dim in 1000 2000 4000 8000 16000 20000
+for dim in 2000 4000 8000 16000 20000
 do
-	for ntasks in 1 2 3 4 5 6 7 8; do
+	for ntasks in 1 2 4 6 8; do
 		echo "Running $ntasks MPI tasks"
 		    
-		output=$(mpirun --map-by $mapping --bind-to $binding -n $ntasks ./build/gol.x -i -k $dim)
+		output=$(mpirun --map-by $mapping -n $ntasks ./build/gol.x -i -e $evolution -k $dim -s 0)
 		    
 	        time=$(echo "$output" | tail -n 1)
 	        echo "$dim,$ntasks,$time" >> "$output_file"
--- Assignment/exercise1/openmp-scalability.sh
+++ Assignment/exercise1/openmp-scalability.sh
@@ -2,60 +2,52 @@
 #SBATCH -A dssc
 #SBATCH --job-name=openmp-scal
 #SBATCH --partition=THIN
+#SBATCH --nodelist=thin001
 #SBATCH --time=02:00:0
 #SBATCH --exclusive
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --output=./outputs/slurm-%j-openmp-scal.txt
 
 module load openMPI/4.1.6/gnu/14.2.1
+# 0 -> ordered, 1 -> static
+evolution=1
+if [evolution==0]; then
+	echo "OpenMP scalability for ordered evolution"
+fi
+if [evolution==1]; then
+	echo "OpenMP scalability for static evolution"
+fi
 
-echo "OpenMP scalability"
 echo "Running game of life with different #threads"
 echo "to collect data for time/threads plots"
 
 output_file="./outputs/timings/openmp-scal-$SLURM_JOB_ID.csv"
-max_num_threads=24 
+max_num_threads=12
 
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+echo "OMP_PLACES = $OMP_PLACES , OMP_PROC_BIND = $OMP_PROC_BIND"
 
-
-# Outer loop repeats the measurements for different matrix dimensions
-# The following dimensions are tested: (100, 500, 1000, 1500, 2000)
-
-if [ "$1" == "-s" ]; then
-    output_file="./outputs/timings/openmp-scal-$SLURM_JOB_ID-serial.csv"
-fi
 echo "dim,nthreads,time" > "$output_file"
-for dim in 1000 1500 2000
+for dim in 1000 2000 4000 8000 16000
 do
-    # Check if single thread flag is provided
-    if [ "$1" == "-s" ]; then
         echo "Using dimension $dim"
-        echo "Running with a single thread"
-
-        export OMP_NUM_THREADS=1
-        output=$(srun ---ntasks-per-socket=1 ./build/gol.x -i -k $dim)
-        time=$(echo "$output" | tail -n 1)
-        echo "$dim,1,$time" >> "$output_file"
-        echo "$output" >> "./outputs/slurm-$SLURM_JOB_ID-serial.txt"
-    else
-        echo "Using dimension $dim"
-        echo "Running with 1 to 64 cores per socket"
+        echo "Running with 1 to 12 cores per socket"
         # Run the loop max_num_threads times
         for ((i=1; i<=$max_num_threads; i+=1))
         do
             echo "Running with $i threads"
             export OMP_NUM_THREADS=$i
             
-            output=$(srun --ntasks-per-socket=1 ./build/gol.x -i -k $dim)
+            output=$(mpirun -n 2 --map-by socket ./build/gol.x -i -e $evolution -k $dim -s 0)
 
             time=$(echo "$output" | tail -n 1)
             echo "$dim,$i,$time" >> "$output_file"
 
             echo "$output" >> "./outputs/slurm-$SLURM_JOB_ID.txt"
         done
-    fi
 done
 
 echo "Finished"
