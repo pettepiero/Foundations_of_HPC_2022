f45608df603145f057c1be79069bf0a1b7daf1b3
Piero <pette.piero@gmail.com>
2025-06-24 16:02:32 +0200

    Cleaned repo and added topo xmls

--- Assignment/exercise1/allocate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Command to allocate resources using salloc
-salloc -N1 -p THIN --ntasks-per-node 1 -A dssc --time=01:00:00
-openMPI/4.1.6/gnu/14.2.1
--- Assignment/exercise1/many-allocs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Command to allocate resources using salloc
-salloc -N2 -p THIN --ntasks-per-node 2 -A dssc --time=01:00:00
-module load openMPI/4.1.6/gnu/14.2.1
